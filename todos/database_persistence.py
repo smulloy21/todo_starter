7031dfe91f87f3376cb13e7141bda9012240cac6
Shannon Mulloy <smulloy21@gmail.com>
2026-04-26 22:48:23 -0400

    prepare for deploy to render

--- todos/database_persistence.py
+++ todos/database_persistence.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 import logging
+import os
 
 import psycopg2
 from psycopg2.extras import DictCursor
@@ -16,7 +17,11 @@ class DatabasePersistence():
 
     @contextmanager
     def _database_connect(self):
-        connection = psycopg2.connect(dbname='todos')
+        if os.environ.get('FLASK_ENV') == 'production':
+            connection = psycopg2.connect(os.environ['DATABASE_URL'])
+        else:
+            connection = psycopg2.connect(dbname="todos")
+
         try:
             with connection:
                 yield connection
